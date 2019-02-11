16a232c54ff027684d88d88902fd549ded1204bb
ShuntaTobe <shuntatobe.hal@gmail.com>
2019-02-12 06:41:09 +0900

    基本的なシェーダ追加
    基本的な頂点、ピクセルシェーダによる三角形ポリゴン描画まで

--- DirectX11_game/main.cpp
+++ DirectX11_game/main.cpp
@@ -1,7 +1,16 @@
 ///////////////////////////////////////////////////////////////////////
 //	DirectX11 ゲーム
-//	作成者：戸部俊太
-//	作成日：2019/2/10
+//	タイトル：main.cpp
+//	作成者	：戸部俊太
+//	作成日	：2019/02/10
+///////////////////////////////////////////////////////////////////////
+//	更新ログ
+//	2019/02/10	・ウィンドウ作成,D3D初期化まで
+//	2019/02/12	・基本的なシェーダ追加,三角ポリゴン表示まで
+///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
+//	インクルードファイル
 ///////////////////////////////////////////////////////////////////////
 #include "main.h"
 
@@ -24,7 +33,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, INT)
 	if (g_pMain != NULL)
 	{
 		if (SUCCEEDED(g_pMain->InitWindow(hInstance,0,0,WINDOW_WIDTH,
-			WINDOW_HEIGHT, (LPSTR)API_NAME)))
+			WINDOW_HEIGHT, (LPSTR)API_NAME)))//	文字セットをマルチバイトに設定してもconst char* から LPSTR　の互換がない　のエラーが発生するのでLPSTRにcastしている
 		{
 			if (SUCCEEDED(g_pMain->InitD3D()))
 			{
@@ -37,9 +46,27 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, INT)
 	return 0;
 }
 
+///////////////////////////////////////////////////////////////////////
+//	ウィンドプロシージャ
+///////////////////////////////////////////////////////////////////////
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	return g_pMain->MsgProc(hWnd, uMsg, wParam, lParam);
+}
+
+///////////////////////////////////////////////////////////////////////
+//	コンストラクタ
+///////////////////////////////////////////////////////////////////////
+CMain::CMain()
+{
+	ZeroMemory(this, sizeof(CMain));
+}
+
+///////////////////////////////////////////////////////////////////////
+//	デストラクタ
+///////////////////////////////////////////////////////////////////////
+CMain::~CMain()
+{
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -76,7 +103,7 @@ HRESULT CMain::InitWindow(HINSTANCE hInstance, INT iX, INT iY, INT iWidth, INT i
 }
 
 ///////////////////////////////////////////////////////////////////////
-//	メッセージ
+//	メッセージプロシージャ
 ///////////////////////////////////////////////////////////////////////
 LRESULT CMain::MsgProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -99,7 +126,7 @@ LRESULT CMain::MsgProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 }
 
 ///////////////////////////////////////////////////////////////////////
-//	メインループ
+//	メインループ処理
 ///////////////////////////////////////////////////////////////////////
 void CMain::Loop()
 {
@@ -123,7 +150,7 @@ void CMain::Loop()
 }
 
 ///////////////////////////////////////////////////////////////////////
-//	アプリケーション
+//	アプリケーション処理
 ///////////////////////////////////////////////////////////////////////
 void CMain::App()
 {
@@ -209,6 +236,135 @@ HRESULT CMain::InitD3D()
 	m_pDevice->CreateRasterizerState(&rdc, &pIr);
 	m_pDeviceContext->RSSetState(pIr);
 
+	//	hlslファイル読み込み	ブロブ作成
+	ID3DBlob *pCompileShader = NULL;
+
+	//	バーテックスシェーダ作成
+	if (FAILED(MakeShader((LPSTR)SIMPLE_SHADER_FILE, (LPSTR)"VS", (LPSTR)"vs_5_0", (void**)&m_pVertexShader, &pCompileShader))) return E_FAIL;
+	//	頂点インプットレイアウト定義
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+	UINT numElements = sizeof(layout) / sizeof(layout[0]);
+
+	//	頂点インプットレイアウト作成
+	if (FAILED(m_pDevice->CreateInputLayout(layout, numElements, pCompileShader->GetBufferPointer(), pCompileShader->GetBufferSize(), &m_pVertexLayout)))
+	{
+		return FALSE;
+	}
+	SAFE_RELEASE(pCompileShader);
+
+	//	ピクセルシェーダ作成
+	if (FAILED(MakeShader((LPSTR)SIMPLE_SHADER_FILE, (LPSTR)"PS", (LPSTR) "ps_5_0", (void**)&m_pPixelShader, &pCompileShader))) return E_FAIL;
+	SAFE_RELEASE(pCompileShader);
+
+	//	コンスタントバッファ作成
+	D3D11_BUFFER_DESC cb;
+	cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cb.ByteWidth = sizeof(D3DXMATRIX);
+	cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cb.MiscFlags = 0;
+	cb.StructureByteStride = 0;
+	cb.Usage = D3D11_USAGE_DYNAMIC;
+
+	if (FAILED(m_pDevice->CreateBuffer(&cb, NULL, &m_pConstantBuffer)))
+	{
+		return E_FAIL;
+	}
+
+	//	ポリゴン、メッシュなどのジオメトリ関連作成
+	if (FAILED(InitModel()))
+	{
+		return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+///////////////////////////////////////////////////////////////////////
+//	hlslファイルからシェーダ作成
+///////////////////////////////////////////////////////////////////////
+HRESULT CMain::MakeShader(LPSTR szFileName, LPSTR szFuncName, LPSTR szProfileName, void** ppShader, ID3DBlob** ppBlob)
+{
+	ID3DBlob *pErrors = NULL;
+
+	if (FAILED(D3DX11CompileFromFileA(szFileName, NULL, NULL, szFuncName, szProfileName, D3D10_SHADER_DEBUG | D3D10_SHADER_SKIP_OPTIMIZATION, 0, NULL, ppBlob, &pErrors, NULL)))
+	{
+		char* p = (char*)pErrors->GetBufferPointer();
+		MessageBox(0, p, 0, MB_OK);
+		return E_FAIL;
+	}
+	SAFE_RELEASE(pErrors);
+
+	char szProfile[3] = { 0 };
+	memcpy(szProfile,szProfileName, 2);
+
+	//	頂点シェーダ
+	if (strcmp(szProfile, "vs") == 0)
+	{
+		if( FAILED(m_pDevice->CreateVertexShader((*ppBlob)->GetBufferPointer(),(*ppBlob)->GetBufferSize(),NULL, (ID3D11VertexShader**)ppShader)))	return E_FAIL;
+	}
+	//	ピクセルシェーダ
+	if (strcmp(szProfile, "ps") == 0)
+	{
+		if (FAILED(m_pDevice->CreatePixelShader((*ppBlob)->GetBufferPointer(), (*ppBlob)->GetBufferSize(), NULL, (ID3D11PixelShader**)ppShader)))	return E_FAIL;
+	}
+	//	ジオメトリシェーダ
+	if (strcmp(szProfile, "gs") == 0)
+	{
+		if (FAILED(m_pDevice->CreateGeometryShader((*ppBlob)->GetBufferPointer(), (*ppBlob)->GetBufferSize(), NULL, (ID3D11GeometryShader**)ppShader)))	return E_FAIL;
+	}
+	//	フルシェーダ
+	if (strcmp(szProfile, "hs") == 0)
+	{
+		if (FAILED(m_pDevice->CreateHullShader((*ppBlob)->GetBufferPointer(), (*ppBlob)->GetBufferSize(), NULL, (ID3D11HullShader**)ppShader)))	return E_FAIL;
+	}
+	//	ドメインシェーダ
+	if (strcmp(szProfile, "ds") == 0)
+	{
+		if (FAILED(m_pDevice->CreateDomainShader((*ppBlob)->GetBufferPointer(), (*ppBlob)->GetBufferSize(), NULL, (ID3D11DomainShader**)ppShader)))	return E_FAIL;
+	}
+	//ComputeShader
+	if (strcmp(szProfile, "cs") == 0)
+	{
+		if (FAILED(m_pDevice->CreateComputeShader((*ppBlob)->GetBufferPointer(), (*ppBlob)->GetBufferSize(), NULL, (ID3D11ComputeShader**)ppShader)))	return E_FAIL;
+	}
+	return S_OK;
+}
+
+
+///////////////////////////////////////////////////////////////////////
+//	ポリゴン、メッシュなどのジオメトリ関連を初期化
+///////////////////////////////////////////////////////////////////////
+HRESULT CMain::InitModel()
+{
+	//	バーテックスバッファ作成
+	SimpleVertex vertices[]=
+	{
+		D3DXVECTOR3(0.0f, 0.5f, 0.0f),
+		D3DXVECTOR3(0.5f, -0.5f, 0.0f),
+		D3DXVECTOR3(-0.5f, -0.5f,0.0f),
+	};
+
+	D3D11_BUFFER_DESC bd;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(SimpleVertex) * 3;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = vertices;
+	if (FAILED(m_pDevice->CreateBuffer(&bd, &InitData, &m_pVertexBuffer)))
+	{
+		return E_FAIL;
+	}
+	//	バーテックスバッファセット
+	UINT stride = sizeof(SimpleVertex);
+	UINT offset = 0;
+	m_pDeviceContext->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &stride, &offset);
+
 	return S_OK;
 }
 
@@ -222,6 +378,49 @@ void CMain::Render()
 	m_pDeviceContext->ClearRenderTargetView(m_pBackBuffer_TexRTV, ClearColor);						//	カラーバッファクリア
 	m_pDeviceContext->ClearDepthStencilView(m_pBuckBuffer_DSTexDSV, D3D11_CLEAR_DEPTH, 1.0f, 0);	//	デプスステンシルバッファクリア
 
+	D3DXMATRIX World;
+	D3DXMATRIX View;
+	D3DXMATRIX Proj;
+	//	ワールドトランスフォーム
+	D3DXMatrixRotationY(&World, timeGetTime() / 100.0f);
+
+	//	ビュートランスフォーム
+	D3DXVECTOR3 vEyePt(0.0f, 1.0f, -2.0f);		//	視点
+	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);	//	注視点
+	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);		//	上方位置
+	D3DXMatrixLookAtLH(&View, &vEyePt, &vLookatPt, &vUpVec);
+
+	//	プロジェクショントランスフォーム
+	D3DXMatrixPerspectiveFovLH(&Proj, D3DX_PI / 4, (FLOAT)WINDOW_WIDTH / (FLOAT)WINDOW_HEIGHT, 0.1f, 100.0f);
+
+	//	シェーダセット
+	m_pDeviceContext->VSSetShader(m_pVertexShader, NULL, 0);
+	m_pDeviceContext->PSSetShader(m_pPixelShader, NULL, 0);
+
+	//	シェーダのコンスタントバッファに各種データを渡す
+	D3D11_MAPPED_SUBRESOURCE pData;
+	SIMPLESHADER_CONSTANT_BUFFER cb;
+	if (SUCCEEDED(m_pDeviceContext->Map(m_pConstantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &pData)))
+	{
+		//	ワールド、カメラ、射影行列を渡す
+		D3DXMATRIX matrix = World * View * Proj;
+		D3DXMatrixTranspose(&matrix, &matrix);
+		cb.mWVP = matrix;
+
+		memcpy_s(pData.pData, pData.RowPitch, (void*)(&cb), sizeof(cb));
+		m_pDeviceContext->Unmap(m_pConstantBuffer, 0);
+	}
+
+	//	このコンスタントバッファをどのシェーダで使用するか
+	m_pDeviceContext->VSSetConstantBuffers(0, 1, &m_pConstantBuffer);
+	m_pDeviceContext->PSSetConstantBuffers(0, 1, &m_pConstantBuffer);
+	//	頂点インプットレイアウトセット
+	m_pDeviceContext->IASetInputLayout(m_pVertexLayout);
+	//	プリミティブ・トポロジーをセット
+	m_pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	//	プリミティブをレンダリング
+	m_pDeviceContext->Draw(3, 0);
+
 	m_pSwapChain->Present(0, 0);	//フロント、バックバッファ切り替え
 }
 
@@ -230,10 +429,15 @@ void CMain::Render()
 ///////////////////////////////////////////////////////////////////////
 void CMain::DestroyD3D()
 {
+	SAFE_RELEASE(m_pConstantBuffer);
+	SAFE_RELEASE(m_pVertexShader);
+	SAFE_RELEASE(m_pPixelShader);
+	SAFE_RELEASE(m_pVertexBuffer);
+	SAFE_RELEASE(m_pVertexLayout);
 	SAFE_RELEASE(m_pSwapChain);
 	SAFE_RELEASE(m_pBackBuffer_TexRTV);
-	SAFE_RELEASE(m_pDeviceContext);
 	SAFE_RELEASE(m_pBuckBuffer_DSTexDSV);
 	SAFE_RELEASE(m_pBuckBuffer_DSTex);
+	SAFE_RELEASE(m_pDeviceContext);
 	SAFE_RELEASE(m_pDevice);
 }
--- DirectX11_game/main.h
+++ DirectX11_game/main.h
@@ -1,45 +1,93 @@
 #pragma once
+///////////////////////////////////////////////////////////////////////
+//	DirectX11 ゲーム
+//	作成者：戸部俊太
+//	作成日：2019/2/10
+///////////////////////////////////////////////////////////////////////
+//	更新ログ
+//	2019/02/10	・ウィンドウ作成,D3D初期化まで
+//	2019/02/12	・基本的なシェーダ追加, 三角ポリゴン表示まで
+///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
 //	インクルードファイル
+///////////////////////////////////////////////////////////////////////
 #include <windows.h>
 #include <d3d11.h>
 #include <D3DX10.h>
 #include <D3DX11.h>
 #include <d3dcompiler.h>
 
+///////////////////////////////////////////////////////////////////////
 //	ライブラリ
-#pragma comment(lib, "d3d11.lib")
+///////////////////////////////////////////////////////////////////////
+#pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "d3dx10.lib")
+#pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
 #pragma comment(lib, "d3dCompiler.lib")
 
+///////////////////////////////////////////////////////////////////////
 //	警告非表示
+///////////////////////////////////////////////////////////////////////
 #pragma warning(disable : 4305)
 
+///////////////////////////////////////////////////////////////////////
+//	マクロ定義
+///////////////////////////////////////////////////////////////////////
+#define SAFE_RELEASE(p) if(p){p->Release(); p=NULL;}
+
+///////////////////////////////////////////////////////////////////////
 //	定数定義
+///////////////////////////////////////////////////////////////////////
 #define WINDOW_WIDTH (640)			//	ウィンドウ幅
 #define WINDOW_HEIGHT (480)			//	ウィンドウ高さ
 #define API_NAME "DirectX11 テスト"	//	アプリケーション名
+#define SIMPLE_SHADER_FILE	"shader/SimpleShader.hlsl"
 
-//	マクロ定義
-#define SAFE_RELEASE(p) if(p){p->Release(); p=NULL;}
+//	頂点構造体
+struct SimpleVertex
+{
+	D3DXVECTOR3 Pos;	//	位置情報
+};
 
-//	メインルーチンクラス
+//	シェーダ用のコンスタントバッファのアプリ側構造体
+struct SIMPLESHADER_CONSTANT_BUFFER
+{
+	D3DXMATRIX mWVP;
+};
+
+///////////////////////////////////////////////////////////////////////
+//	CMain
+//	メインウィンドウクラス
+///////////////////////////////////////////////////////////////////////
 class CMain
 {
 public:
+	CMain();
+	~CMain();
 	HRESULT InitWindow(HINSTANCE, INT, INT, INT, INT, LPSTR);
 	HRESULT InitD3D();
+	HRESULT InitModel();
+	HRESULT MakeShader(LPSTR, LPSTR, LPSTR, void**, ID3DBlob**);
 	LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
 	void Loop();
 	void App();
 	void Render();
 	void DestroyD3D();
 
-	HWND m_hWnd;	//	ウィンドウ情報
-	ID3D11Device* m_pDevice;
-	ID3D11DeviceContext* m_pDeviceContext;
-	IDXGISwapChain* m_pSwapChain;
-	ID3D11Texture2D* m_pBuckBuffer_DSTex;
-	ID3D11RenderTargetView* m_pBackBuffer_TexRTV;
-	ID3D11DepthStencilView* m_pBuckBuffer_DSTexDSV;
+	//	メンバ変数
+	HWND					m_hWnd;						//	ウィンドウ情報
+	ID3D11Device*			m_pDevice;					//	D3Dデバイス
+	ID3D11DeviceContext*	m_pDeviceContext;			//	デバイスコンテキスト
+	IDXGISwapChain*			m_pSwapChain;				//	スワップチェイン
+	ID3D11Texture2D*		m_pBuckBuffer_DSTex;		//	2Dテクスチャ
+	ID3D11RenderTargetView*	m_pBackBuffer_TexRTV;		//	レンダターゲット
+	ID3D11DepthStencilView*	m_pBuckBuffer_DSTexDSV;		//	ステンシルビュー
+
+	ID3D11InputLayout*		m_pVertexLayout;
+	ID3D11VertexShader*		m_pVertexShader;			//	頂点シェーダー
+	ID3D11PixelShader*		m_pPixelShader;				//	ピクセルシェーダー
+	ID3D11Buffer*			m_pConstantBuffer;
+	ID3D11Buffer*			m_pVertexBuffer;			//	頂点バッファ
 };
